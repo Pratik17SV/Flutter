2680bde5d156fabc380fb0a4789989e0d80892a7
PratikSV <pratik.dg@somaiya.edu>
2025-03-08 23:46:03 +0530

    Change color theme to dark
    use brightness: Brightness.dark,

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -37,7 +37,10 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+          brightness: Brightness.dark, //Change the color theme to dark
+        ),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
