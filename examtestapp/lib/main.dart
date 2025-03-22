d1e1338bc9f3302016d70ce3833e7de797ec937f
PratikSV <pratikgaikwad7738@gmail.com>
2025-03-22 20:11:23 +0530

    navigation example is done

--- examtestapp/lib/main.dart
+++ examtestapp/lib/main.dart
@@ -7,18 +7,69 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  //Custom function for on tap event
-  void Usertap() {
-    print('User tapped');
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner:
-          false, //Remove the banner in the nav on top right
+      debugShowCheckedModeBanner: false,
+      home: FirstPage(),
+      routes: {
+        '/firstpage': (context) => FirstPage(),
+        '/secondpage': (context) => SecondPage(),
+      },
+    );
+  }
+}
 
-      home: Scaffold(),
+//==================================================================================================================
+//First page
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: Center(
+          child: Text('1st Page.', style: TextStyle(color: Colors.white)),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            //Used to navigate through button
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondPage()),
+            );
+          },
+          child: Text('Navigate to second Page.'),
+        ),
+      ),
+    );
+  }
+}
+
+//==================================================================================================================
+//Second Page
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Center(child: Text('2nd Page'))),
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            //Used to navigate through button
+            Navigator.pushNamed(context, '/firstpage');
+          },
+          child: Text('Navigate to second Page.'),
+        ),
+      ),
     );
   }
 }
